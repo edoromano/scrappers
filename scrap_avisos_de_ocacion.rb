60d7c8488b5671f89bebb3097d29fb44e4939ae8
Eduardo Romano <eduardo.romano.ortiz@autodesk.com>
2016-11-10 00:41:14 -0600

    Update to map properties

--- scrap_avisos_de_ocacion.rb
+++ scrap_avisos_de_ocacion.rb
@@ -31,8 +31,16 @@ class Scrap
       bathrooms = table.search("table.ar12gris/tr[2]/td/table/tr[3]/td[1]").text.split.first
       latitude = table.search("table.ar12gris/tr[2]/td/table/tr[4]/td[3]/a[class='tool']").first.attributes['onclick'].value.split(",")[3].gsub(/[']/,"")
       longitude = table.search("table.ar12gris/tr[2]/td/table/tr[4]/td[3]/a[class='tool']").first.attributes['onclick'].value.split(",")[4].gsub(/[']/,"")
-      description = "colonia: "+neighborhood+" ciudad: "+city+" pisos: "+floors+" area: "+area
-      house = House.new(:price => price, :user_id => 1, :latitude => latitude, :longitude => longitude, :description => description )
+      house = House.new(:price => price,
+                        :user_id => 1,
+                        :neighborhood => neighborhood,
+                        :city => city,
+                        :floors => floors,
+                        :area => area,
+                        :rooms =>rooms,
+                        :bathrooms => bathrooms,
+                        :latitude => latitude,
+                        :longitude => longitude)
       house.save
       rescue Exception => exc
         p "exc #{exc.message}"
@@ -42,7 +50,7 @@ class Scrap
 end
 
 class House < ActiveRecord::Base
-  validates :price, :description, :user_id, presence: true
+  validates :price, :user_id, presence: true
   validates :latitude, numericality: { greater_than_or_equal_to: 0 }, presence: true
   validates :longitude, numericality: { less_than_or_equal_to: 0 }, presence: true
 end
