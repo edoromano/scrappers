cbb26d3e168b30c2c0b527118dc5bb3f9c588dbb
Eduardo Romano <edo.romanoo@gmail.com>
2016-11-12 10:01:04 -0600

    Modify to include all `fields` of interest

--- scrap_avisos_de_ocacion_df.rb
+++ scrap_avisos_de_ocacion_df.rb
@@ -31,8 +31,16 @@ class Scrap
         bathrooms = table.search("table.ar12gris/tr[2]/td/table/tr[3]/td[1]").text.split.first
         latitude = table.search("table.ar12gris/tr[2]/td/table/tr[4]/td[3]/a[class='tool']").first.attributes['onclick'].value.split(",")[3].gsub(/[']/,"")
         longitude = table.search("table.ar12gris/tr[2]/td/table/tr[4]/td[3]/a[class='tool']").first.attributes['onclick'].value.split(",")[4].gsub(/[']/,"")
-        description = "colonia: "+neighborhood+" ciudad: "+city+" pisos: "+floors+" area: "+area
-        house = House.new(:price => price, :user_id => 1, :latitude => latitude, :longitude => longitude, :description => description )
+        house = House.new(:price => price,
+                          :user_id => 1,
+                          :neighborhood => neighborhood,
+                          :city => city,
+                          :floors => floors,
+                          :area => area,
+                          :rooms =>rooms,
+                          :bathrooms => bathrooms,
+                          :latitude => latitude,
+                          :longitude => longitude)
         house.save
       rescue Exception => exc
         p "exc #{exc.message}"
@@ -42,12 +50,12 @@ class Scrap
 end
 
 class House < ActiveRecord::Base
-  validates :price, :description, :user_id, presence: true
+  validates :price, :user_id, presence: true
   validates :latitude, numericality: { greater_than_or_equal_to: 0 }, presence: true
   validates :longitude, numericality: { less_than_or_equal_to: 0 }, presence: true
 end
 
-(1..102).each do |page|
+(1..130).each do |page|
 #  Scrap.new("http://www.avisosdeocasion.com/Resultados-Inmuebles.aspx?n=venta-casas-nuevo-leon&PlazaBusqueda=2&Plaza=2&pagina=#{page}&idinmueble=3")
   Scrap.new("http://www.avisosdeocasion.com/reforma/venta/casas/casas.aspx?ntext=venta-casas-casas-Distrito-Federal&PlazaBusqueda=1&Plaza=1&pagina=#{page}")
 end
